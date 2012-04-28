ad92eeefc422067cf440533cba535eb59229427e
Dan P. Smith <mrdanpsmith@gmail.com>
2012-04-28 12:55:29 -0400

    Started stubbing out the app.

--- markleter.rb
+++ markleter.rb
@@ -1,7 +1,17 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
+require 'yui/compressor'
+
+set :haml, :format => :html5
 
 get '/' do
-	haml :index, :format => :html5
+	haml :index
+end
+
+post '/markletize' do
+	marklet = params[:marklet] 
+	compressor = YUI::JavaScriptCompressor.new(:munge => false)
+	minned = compressor.compress(marklet)
+	haml :marklet, :locals => {:minned => minned}
 end
